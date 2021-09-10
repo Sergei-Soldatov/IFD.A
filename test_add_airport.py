cfe2bd5a4c25cc647fb20326dfebad4a4bfa78be
s.shulga <s.shulga@rivc-pulkovo.ru>
2021-09-10 12:28:09 +0300

    Замена названия переменной driver, которую создает Katalon Recorder выделили эту переменную в методе def setUp(self) и def test_add_airport(self)
    driver меняем на wd (сокращенно от веб драйвер) инструментом IDE: Refactor -> Rename

--- test_add_airport.py
+++ test_add_airport.py
@@ -9,55 +9,43 @@ import unittest, time, re
 
 class TestAddAirport(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
 
     def test_add_airport(self):
-        driver = self.driver
-        driver.get("http://192.168.178.197/login")
-        driver.find_element_by_id("f_c845c713-4f14-44d5-9e78-737feb46b176").click()
-        driver.find_element_by_id("f_c845c713-4f14-44d5-9e78-737feb46b176").clear()
-        driver.find_element_by_id("f_c845c713-4f14-44d5-9e78-737feb46b176").send_keys("test")
-        driver.find_element_by_id("f_03342e34-3fbb-45da-a7b2-1e7c35463693").click()
-        driver.find_element_by_id("f_03342e34-3fbb-45da-a7b2-1e7c35463693").clear()
-        driver.find_element_by_id("f_03342e34-3fbb-45da-a7b2-1e7c35463693").send_keys("1245")
-        driver.find_element_by_xpath("//form[@id='login-card']/div[3]/button/span[2]/span/span").click()
-        driver.find_element_by_xpath("//div[@id='q-app']/div/header/div/button/span[2]/span/i").click()
-        driver.find_element_by_xpath("//div[@id='q-app']/div/div/aside/div/div[2]/div[2]/div[3]/span").click()
-        driver.find_element_by_xpath("//div[@id='q-app']/div/div[2]/div/div/div/div/div/div/div[2]").click()
-        driver.find_element_by_xpath("//div[@id='q-app']/div/div[2]/div/div[2]/div[2]/div/button/span[2]/span").click()
-        driver.find_element_by_id("f_7fc25f94-7cba-469d-a730-ac9eae672d42").click()
-        driver.find_element_by_id("f_7fc25f94-7cba-469d-a730-ac9eae672d42").clear()
-        driver.find_element_by_id("f_7fc25f94-7cba-469d-a730-ac9eae672d42").send_keys("LED")
-        driver.find_element_by_xpath(u"(.//*[normalize-space(text()) and normalize-space(.)='Код ИАТА'])[1]/following::span[2]").click()
-        driver.find_element_by_css_selector("path.q-checkbox__truthy").click()
-        driver.find_element_by_xpath(u"(.//*[normalize-space(text()) and normalize-space(.)='Назад'])[1]/following::span[4]").click()
-        driver.find_element_by_xpath("//div[@id='q-app']/div/header/div/div/div[2]/div[4]/button/span[2]/span/i").click()
+        wd = self.wd
+        wd.get("http://192.168.178.197/login")
+        wd.find_element_by_id("f_c845c713-4f14-44d5-9e78-737feb46b176").click()
+        wd.find_element_by_id("f_c845c713-4f14-44d5-9e78-737feb46b176").clear()
+        wd.find_element_by_id("f_c845c713-4f14-44d5-9e78-737feb46b176").send_keys("test")
+        wd.find_element_by_id("f_03342e34-3fbb-45da-a7b2-1e7c35463693").click()
+        wd.find_element_by_id("f_03342e34-3fbb-45da-a7b2-1e7c35463693").clear()
+        wd.find_element_by_id("f_03342e34-3fbb-45da-a7b2-1e7c35463693").send_keys("1245")
+        wd.find_element_by_xpath("//form[@id='login-card']/div[3]/button/span[2]/span/span").click()
+        wd.find_element_by_xpath("//div[@id='q-app']/div/header/div/button/span[2]/span/i").click()
+        wd.find_element_by_xpath("//div[@id='q-app']/div/div/aside/div/div[2]/div[2]/div[3]/span").click()
+        wd.find_element_by_xpath("//div[@id='q-app']/div/div[2]/div/div/div/div/div/div/div[2]").click()
+        wd.find_element_by_xpath("//div[@id='q-app']/div/div[2]/div/div[2]/div[2]/div/button/span[2]/span").click()
+        wd.find_element_by_id("f_7fc25f94-7cba-469d-a730-ac9eae672d42").click()
+        wd.find_element_by_id("f_7fc25f94-7cba-469d-a730-ac9eae672d42").clear()
+        wd.find_element_by_id("f_7fc25f94-7cba-469d-a730-ac9eae672d42").send_keys("LED")
+        wd.find_element_by_xpath(u"(.//*[normalize-space(text()) and normalize-space(.)='Код ИАТА'])[1]/following::span[2]").click()
+        wd.find_element_by_css_selector("path.q-checkbox__truthy").click()
+        wd.find_element_by_xpath(u"(.//*[normalize-space(text()) and normalize-space(.)='Назад'])[1]/following::span[4]").click()
+        wd.find_element_by_xpath("//div[@id='q-app']/div/header/div/div/div[2]/div[4]/button/span[2]/span/i").click()
     
     def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
+        try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
     
     def is_alert_present(self):
-        try: self.driver.switch_to_alert()
+        try: self.wd.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
     
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
     def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
+        self.wd.quit()
 
 if __name__ == "__main__":
     unittest.main()
