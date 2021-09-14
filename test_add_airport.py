ce4b00c9edaeb3e24882fe4defb764c366c4e59f
s.shulga <s.shulga@rivc-pulkovo.ru>
2021-09-14 16:25:23 +0300

    Жмякаем кнопку выход (работают три способа). В методе def setUp(self): "играем" со временем для закрытия страницы

--- test_add_airport.py
+++ test_add_airport.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 import unittest, time, re
 
 class TestAddAirport(unittest.TestCase):
     def setUp(self):
 #        self.wd = webdriver.Chrome()
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+        self.wd.implicitly_wait(5)
 
     def test_add_airport(self):
         # Открытие страницы
@@ -25,7 +28,7 @@ class TestAddAirport(unittest.TestCase):
         wd.find_element_by_xpath("//form[@id='login-card']/div[2]/label[2]/div/div/div/input").clear()
         wd.find_element_by_xpath("//form[@id='login-card']/div[2]/label[2]/div/div/div/input").send_keys("1245")
 
-        # Клик по кнопке "Войти"
+        # Валидация логина и пароля (кнопка "Войти")
         wd.find_element_by_xpath("//form[@id='login-card']/div[3]/button/span[2]/span/span").click()
 
         # Открытие модуля "Справочники/Аэропорты"
@@ -46,14 +49,15 @@ class TestAddAirport(unittest.TestCase):
         wd.find_element_by_css_selector("svg.q-checkbox__svg.fit.absolute-full").click()
         wd.find_element_by_xpath(u"(.//*[normalize-space(text()) and normalize-space(.)='Назад'])[1]/following::span[4]").click()
 
-        #wd.implicitly_wait(30)
+        # Жмякаем кнопку выход (работают три способа). В методе def setUp(self): "играем" со временем
+        #element = wd.find_element_by_xpath("//div[@id='q-app']/div/header/div/div/div[2]/div[4]/button/span[2]/span/i")
+        #wd.execute_script("arguments[0].click();", element)
 
-        #wd.quit()
-        #wd.find_element_by_xpath("//div[@id='q-app']/div/header/div/div/div/div/i").click()
-        #wd.find_element_by_xpath("//div[4]/button/span[2]/span/i").click()
-        #wd.find_element_by_xpath("//div[@id='q-app']/div/header/div/button/span[2]/span/i").click()
-        #wd.find_element_by_xpath("//div[@id='q-app']/div/header/div/button/span[2]/span/i").click()
-        #wd.find_element_by_xpath("//div[@id='q-app']/div/header/div/div/div[2]/div[4]/button/span[2]/span/i").click()
+        #element = wd.find_element_by_xpath("//div[@id='q-app']/div/header/div/div/div[2]/div[4]/button/span[2]/span/i")
+        #webdriver.ActionChains(wd).move_to_element(element).click(element).perform()
+
+        WebDriverWait(wd, 10).until(EC.invisibility_of_element((By.XPATH, "//div[@class='q-dialog__backdrop fixed-full q-transition--fade-leave-active q-transition--fade-leave-to']")))
+        WebDriverWait(wd, 10).until(EC.element_to_be_clickable((By.XPATH, "//i[@class='mdi mdi-exit-to-app q-icon notranslate']"))).click()
 
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
