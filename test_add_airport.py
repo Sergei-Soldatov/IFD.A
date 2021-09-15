7a9c22bf0a5ad3a077fdf1ed8b5e8f3668377226
s.shulga <s.shulga@rivc-pulkovo.ru>
2021-09-15 14:33:54 +0300

    Рефакторинг кода

--- test_add_airport.py
+++ test_add_airport.py
@@ -13,52 +13,94 @@ class TestAddAirport(unittest.TestCase):
 
     def test_add_airport(self):
         wd = self.wd
-        # Открытие домашней страницы и максимальное разворачивание окна
-        wd.get("http://192.168.178.197/")
-        wd.maximize_window()
+        self.open_home_page(wd)
 
-        # Ввод логина
-        wd.find_element_by_xpath("//form[@id='login-card']/div[2]/label/div/div/div/input").click()
-        wd.find_element_by_xpath("//form[@id='login-card']/div[2]/label/div/div/div/input").clear()
-        wd.find_element_by_xpath("//form[@id='login-card']/div[2]/label/div/div/div/input").send_keys("test")
+        self.enter_login(wd)
+        self.enter_password(wd)
+        self.confurm_login_and_password(wd)
+
+        self.open_sandwich_menu(wd)
+        self.airport_module_selection(wd)
+        self.open_tab_airport(wd)
+
+        self.open_form_add_airport(wd)
+
+        self.enter_IATA_code(wd)
+
+        self.search_airport_by_parameter(wd)
+        self.activat_checkbox_for_airport(wd)
+        self.adding_found_airport(wd)
+
+        self.logout(wd)
+
+    def logout(self, wd):
+        # Выполнение logout (первый способ). В методе def setUp(self): "играем" со временем
+        # element = wd.find_element_by_xpath("//div[@id='q-app']/div/header/div/div/div[2]/div[4]/button/span[2]/span/i")
+        # wd.execute_script("arguments[0].click();", element)
+        # Выполнение logout (второй способ). В методе def setUp(self): "играем" со временем
+        # element = wd.find_element_by_xpath("//div[@id='q-app']/div/header/div/div/div[2]/div[4]/button/span[2]/span/i")
+        # webdriver.ActionChains(wd).move_to_element(element).click(element).perform()
+        # Выполнение logout (третий способ). В методе def setUp(self): "играем" со временем
+        WebDriverWait(wd, 7).until(EC.invisibility_of_element((By.XPATH,
+                                                               "//div[@class='q-dialog__backdrop fixed-full q-transition--fade-leave-active q-transition--fade-leave-to']")))
+        WebDriverWait(wd, 7).until(
+            EC.element_to_be_clickable((By.XPATH, "//i[@class='mdi mdi-exit-to-app q-icon notranslate']"))).click()
+
+    def adding_found_airport(self, wd):
+        # Добавление найденного аэропорта (кнопка "Добавить")
+        wd.find_element_by_xpath(
+            u"(.//*[normalize-space(text()) and normalize-space(.)='Назад'])[1]/following::span[4]").click()
+
+    def activat_checkbox_for_airport(self, wd):
+        # Активация чекбокса найденного аэропорта (поле "Выберите аэропорт")
+        wd.find_element_by_css_selector("svg.q-checkbox__svg.fit.absolute-full").click()
+
+    def search_airport_by_parameter(self, wd):
+        # Поиск аэропорта по введенным параметрам (кнопка "Поиск")
+        wd.find_element_by_xpath(
+            u"(.//*[normalize-space(text()) and normalize-space(.)='Код ИАТА'])[1]/following::span[4]").click()
+
+    def enter_IATA_code(self, wd):
+        # Ввод кода ИАТА в поле "Код ИАТА" в форме "Добавить аэропорт"
+        wd.find_element_by_xpath("//div[4]/label/div/div/div/input").click()
+        wd.find_element_by_xpath("//div[4]/label/div/div/div/input").send_keys("LED")
+
+    def open_form_add_airport(self, wd):
+        # Открытие формы "Добавить Аэропорт" (кнопка "+Добавить")
+        wd.find_element_by_xpath("//div[@id='q-app']/div/div[2]/div/div[2]/div[2]/div/button/span[2]/span").click()
+
+    def open_tab_airport(self, wd):
+        # Открытие вкладки "Аэропорты"
+        wd.find_element_by_xpath("//div[@id='q-app']/div/div[2]/div/div/div/div/div/div/div[2]").click()
+
+    def airport_module_selection(self, wd):
+        # Выбор модуля "Справочники/Аэропорты"
+        wd.find_element_by_xpath("//div[@id='q-app']/div/div/aside/div/div[2]/div[3]/div[3]/span").click()
+
+    def open_sandwich_menu(self, wd):
+        # Открытие сэндвич-меню выбора модулей
+        wd.find_element_by_xpath("//div[@id='q-app']/div/header/div/button/span[2]/span/i").click()
+
+    def confurm_login_and_password(self, wd):
+        # Валидация логина и пароля (кнопка "Войти")
+        wd.find_element_by_xpath("//form[@id='login-card']/div[3]/button/span[2]/span/span").click()
+
+    def enter_password(self, wd):
         # Ввод пароля
         wd.find_element_by_xpath("//form[@id='login-card']/div[2]/label[2]/div/div/div/input").click()
         wd.find_element_by_xpath("//form[@id='login-card']/div[2]/label[2]/div/div/div/input").clear()
         wd.find_element_by_xpath("//form[@id='login-card']/div[2]/label[2]/div/div/div/input").send_keys("1245")
-        # Валидация логина и пароля (кнопка "Войти")
-        wd.find_element_by_xpath("//form[@id='login-card']/div[3]/button/span[2]/span/span").click()
 
-        # Открытие сэндвич-меню выбора модулей
-        wd.find_element_by_xpath("//div[@id='q-app']/div/header/div/button/span[2]/span/i").click()
-        # Выбор модуля "Справочники/Аэропорты"
-        wd.find_element_by_xpath("//div[@id='q-app']/div/div/aside/div/div[2]/div[3]/div[3]/span").click()
-        # Открытие вкладки "Аэропорты"
-        wd.find_element_by_xpath("//div[@id='q-app']/div/div[2]/div/div/div/div/div/div/div[2]").click()
+    def enter_login(self, wd):
+        # Ввод логина
+        wd.find_element_by_xpath("//form[@id='login-card']/div[2]/label/div/div/div/input").click()
+        wd.find_element_by_xpath("//form[@id='login-card']/div[2]/label/div/div/div/input").clear()
+        wd.find_element_by_xpath("//form[@id='login-card']/div[2]/label/div/div/div/input").send_keys("test")
 
-        # Открытие формы "Добавить Аэропорт" (кнопка "+Добавить")
-        wd.find_element_by_xpath("//div[@id='q-app']/div/div[2]/div/div[2]/div[2]/div/button/span[2]/span").click()
-
-        # Ввод кода ИАТА
-        wd.find_element_by_xpath("//div[4]/label/div/div/div/input").click()
-        wd.find_element_by_xpath("//div[4]/label/div/div/div/input").send_keys("LED")
-
-        # Поиск аэропорта по введенным параметрам (кнопка "Поиск")
-        wd.find_element_by_xpath(u"(.//*[normalize-space(text()) and normalize-space(.)='Код ИАТА'])[1]/following::span[4]").click()
-        # Активация чекбокса найденного аэропорта (поле "Выберите аэропорт")
-        wd.find_element_by_css_selector("svg.q-checkbox__svg.fit.absolute-full").click()
-        # Добавление найденного аэропорта (кнопка "Добавить")
-        wd.find_element_by_xpath(u"(.//*[normalize-space(text()) and normalize-space(.)='Назад'])[1]/following::span[4]").click()
-
-        # Выполнение logout (первый способ). В методе def setUp(self): "играем" со временем
-        #element = wd.find_element_by_xpath("//div[@id='q-app']/div/header/div/div/div[2]/div[4]/button/span[2]/span/i")
-        #wd.execute_script("arguments[0].click();", element)
-        # Выполнение logout (второй способ). В методе def setUp(self): "играем" со временем
-        #element = wd.find_element_by_xpath("//div[@id='q-app']/div/header/div/div/div[2]/div[4]/button/span[2]/span/i")
-        #webdriver.ActionChains(wd).move_to_element(element).click(element).perform()
-
-        # Выполнение logout (третий способ). В методе def setUp(self): "играем" со временем
-        WebDriverWait(wd, 7).until(EC.invisibility_of_element((By.XPATH, "//div[@class='q-dialog__backdrop fixed-full q-transition--fade-leave-active q-transition--fade-leave-to']")))
-        WebDriverWait(wd, 7).until(EC.element_to_be_clickable((By.XPATH, "//i[@class='mdi mdi-exit-to-app q-icon notranslate']"))).click()
+    def open_home_page(self, wd):
+        # Открытие домашней страницы и максимальное разворачивание окна
+        wd.get("http://192.168.178.197/")
+        wd.maximize_window()
 
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
